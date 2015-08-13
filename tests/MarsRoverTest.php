f0c68c9b144332755a905316d67e4b09f63e9631
zingorn <zingorn@gmail.com>
2015-08-13 14:03:32 +0300

    init

--- tests/MarsRoverTest.php
+++ tests/MarsRoverTest.php
@@ -23,14 +23,15 @@ class MarsRoverTest extends PHPUnit_Framework_TestCase
                 continue;
             }
 
-            if (!preg_match('/^([\d]+)\.test$/', $file, $fileName)) {
+            if (!preg_match('/^([\d]+)\.(in|out)$/', $file, $fileName)) {
                 continue;
             }
 
             $testName = $fileName[1];
-            $data[$testName] = array(
-                'input' => file_get_contents($filePath)
-            );
+            if (!isset($data[$testName])) {
+                $data[$testName] = array('in' => null, 'out' => array());
+            }
+            $data[$testName][$fileName[2]] = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         }
 
         return $data;
@@ -41,12 +42,57 @@ class MarsRoverTest extends PHPUnit_Framework_TestCase
      * @param $data
      * @return void
      */
-    public function testRoverMoveProgram($input)
+    public function testRoverMoveProgram($input, $output)
     {
+        list($maxX, $maxY) = explode(' ', array_shift($input));
         $context = new \Nasa\Program\Context\PlatoContext();
-        $program = new \Nasa\Program\MoveProgram();
-        $program->
-        var_dump($input);die;
-        //$this->assertTrue($data);
+        $scope = new \Nasa\Model\Coordinate\CartesianScope();
+        $scope->setOptions(array(
+            'maxScopeX' => $maxX,
+            'maxScopeY' => $maxY,
+        ));
+        $context->setScope($scope);
+
+        $testCount = 1;
+        $outputs = array();
+        while (count($input)) {
+            $position = array_shift($input);
+            $line = array_shift($input);
+
+            $testTitle = sprintf('#%s: %s and program %s ', $testCount, $position, $line);
+            $program = new \Nasa\Program\MoveProgram();
+            $program->setContext($context);
+            if ($position === null || $line === null) {
+                $this->fail(sprintf('Wrong test config? Position: "%s"; Programm: "%s":  ' ,$position, $line));
+            }
+            $position = explode(' ', $position);
+            if (count($position) != 3) {
+                $this->fail('Invalid position string? Given: ' . implode(' ', $position));
+            }
+            $testOutput = array_shift($output);
+            if ($testOutput === null) {
+                $this->fail(sprintf('Wrong output string for test %s',$testTitle));
+            }
+
+            list($x, $y, $direction) = $position;
+
+            $rover = new \Nasa\Model\Rover\Rover();
+            $rover->setDirection(\Nasa\Model\Direction\CardinalDirection::createByAlias($direction));
+
+            $roverPosition = new \Nasa\Model\Coordinate\CartesianCoordinate();
+            $roverPosition->setX($x)->setY($y);
+            $rover->setPosition($roverPosition);
+
+            $program->setInput($line);
+            $program->run($rover);
+
+            if ($testOutput !== $program->getOutput()) {
+                $this->fail(sprintf('Fail output of program for test %s. Expected %s, given %s.',$testTitle, $testOutput, $program->getOutput()));
+            }
+            echo "\n" . $program->getOutput();
+            $testCount++;
+        }
+
+        $this->assertTrue(true);
     }
 }
